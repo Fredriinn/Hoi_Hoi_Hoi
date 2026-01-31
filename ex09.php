7c0f03c58ba24123bbbb45dde277e5ed105aa7b5
Yamikase <oppvkn677@gmail.com>
2026-01-31 14:13:28 +0700

    Hola

---
+++ ex09.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<h1>Test Index Array</h1>
+<?php
+$Numbers = [3000,5000,7000];
+//Display
+echo("<br><br>");
+foreach($Numbers as $index => $value){
+echo("$index - $value<br>");
+}
+?>
+</body>
+</html>
